615eb8438915e8f236349ed3aef70e3c9a66540c
ttnaoto <ttnaoto@gmail.com>
2021-03-19 16:45:03 +0900

    remove: Listの方で値の変更などを管理しているので、TodoItemViewModelでは値を保持するだけでいい。なので、そこの部分を変更

--- app/src/main/java/com/example/practicedagger/ui/tile/TodoItemViewModel.kt
+++ app/src/main/java/com/example/practicedagger/ui/tile/TodoItemViewModel.kt
@@ -1,33 +1,21 @@
 package com.example.practicedagger.ui.tile
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.practicedagger.data.Todo
 import com.example.practicedagger.ui.TodoListViewModel
 
 class TodoItemViewModel(
-    todo_item: Todo,
+    val todo: Todo,
     private val todoListViewModel: TodoListViewModel
 ) : ViewModel() {
-    private val _todo = MutableLiveData<Todo>()
-    val todo: LiveData<Todo>
-        get() = _todo
-
-    init {
-        _todo.value = todo_item
-    }
-
     fun changeDone() {
-        val item = todo.value ?: throw NullPointerException("TodoItemViewModel has no data")
-        _todo.value = item.copy(done = !item.done)
         todoListViewModel.change(
-            _todo.value ?: throw java.lang.NullPointerException("Todo Is Null")
+            todo.copy(done = !todo.done)
         )
     }
 
     fun remove() {
-        todoListViewModel.delete(todo.value ?: return)
+        todoListViewModel.delete(todo)
     }
 
 }
